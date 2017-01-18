42aa1be52a6e4c9cff080d77371b3cad0c05585e
Jibran Ibrahim Syed <jisyed14@gmail.com>
2017-01-17 21:25:22 -0600

    Uploading initial Xcode project, but main()’s need to be consolidated somehow

---
+++ platform/macos/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GraphicsSandboxApp
+//
+//  Created by Jibran Syed on 1/17/17.
+//  Copyright © 2017 Jibran Syed. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ platform/macos/main.mm
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  GraphicsSandboxApp
+//
+//  Created by Jibran Syed on 1/17/17.
+//  Copyright © 2017 Jibran Syed. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
