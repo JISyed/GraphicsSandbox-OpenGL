06d594855de66dc14f332fadbf5b60afaf4f952b
Jibran Syed <jisyed14@gmail.com>
2017-01-19 17:18:10 -0600

    Added post-build script in Windows project to automatically copy the DLL files into the build folders

--- platform/windows/Copy_DLLs_to_Builds.bat
+++ platform/windows/Copy_DLLs_to_Builds.bat
@@ -1,7 +1,9 @@
+echo "Copying DLLs into Debug build..."
 IF exist Debug\ ( xcopy /R /Y .\..\..\thirdparty\freetype\freetype6.dll Debug\ )
 IF exist Debug\ ( xcopy /R /Y .\..\..\thirdparty\irrklang\irrklang.dll Debug\ )
 IF exist Debug\ ( xcopy /R /Y .\..\..\thirdparty\irrklang\ikpMP3.dll Debug\ )
 IF exist Debug\ ( xcopy /R /Y .\..\..\thirdparty\oail\assimp-vc120-mt.dll Debug\ )
+echo "Copying DLLS into Release build..."
 IF exist Release\ ( xcopy /R /Y .\..\..\thirdparty\freetype\freetype6.dll Release\ )
 IF exist Release\ ( xcopy /R /Y .\..\..\thirdparty\irrklang\irrklang.dll Release\ )
 IF exist Release\ ( xcopy /R /Y .\..\..\thirdparty\irrklang\ikpMP3.dll Release\ )
